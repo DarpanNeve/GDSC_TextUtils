95b8d5faa40765a69e87d00d63786f8a4836e02a
Darpan Neve <darpanneve3@gmail.com>
2024-04-15 17:15:04 +0530

    new added

--- src/App.js
+++ src/App.js
@@ -1,73 +1,24 @@
-
-// import logo from './logo.svg';
-import './App.css';
-import About from './components/About';
-import Navbar from './components/Navbar';
-import React, {useState} from 'react';
-
-import TextForm from './components/TextForm';
-import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import Legal from "./Pages/Legal";
+import NotFound from "./Pages/NotFound";
+import Appointment from "./Pages/Appointment";
 
 function App() {
-  const [mode,setMode] = useState('light');
-  const [alert, setalert] = useState(null);
-
-  const showalert=(message,type)=>
-  {
-    setalert(
-      {
-        msg:message,
-        type:type,
-      }
-      )
-      setTimeout(() => {
-        setalert(null);
-      }, 1000);
-  }
-
-  const togglemode = ()=>
-  {
-    if(mode === 'light')
-    {
-      setMode('dark');
-      document.body.style.backgroundColor="#062f56";
-      document.body.style.color="white";
-      showalert("Dark Mode is activated","success");
-      
-    }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor="white";
-      document.body.style.color="#062f56";
-      showalert("Light Mode is activated","success");
-    }
-  }
   return (
-    <Router>
-      <div>
-        <Navbar title="TEXT-UTILS" about="About Us" mode={mode} togglemode={togglemode} />
-        <Alert alert={alert} />
-        <div className="container my-3">
-          <Switch>
-            <Route path="/about">
-              <About mode={mode}/>
-            </Route>
-            <Route path="/">
-              <TextForm heading="Enter Text to Analyze" showalert={showalert} mode={mode}/>
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </Router>
+    <div className="App">
+      <Router basename="/Health-Plus">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/legal" element={<Legal />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
-
 
 export default App;
